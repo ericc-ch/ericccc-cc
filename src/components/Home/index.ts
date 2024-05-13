5dcc3ae98bab56f06503d6d37ce17435aad846f7
Erick Christian <erickchristian48@gmail.com>
2024-05-13 17:31:56 +0700

    feat: Bookmarks

--- src/components/Home/index.ts
+++ src/components/Home/index.ts
@@ -1,3 +1,4 @@
 export * from "./Blogs";
 export { default as Fullname } from "./Fullname.astro";
 export { default as Projects } from "./Projects.astro";
+export { default as Bookmarks } from "./Bookmarks.astro";
