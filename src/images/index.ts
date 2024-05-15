a70dbf993ea9fdedcf31cd49647946a61cea71f7
Erick Christian <erickchristian48@gmail.com>
2024-05-15 15:54:37 +0700

    feat: finish homescreen UI

---
+++ src/images/index.ts
@@ -0,0 +1,11 @@
+import protos from "./logo-protos.svg";
+import skilledd from "./logo-skilledd.svg";
+import wikrama from "./logo-wikrama.webp";
+import xcidic from "./logo-xcidic.svg";
+
+export const LOGOS = {
+  protos,
+  skilledd,
+  wikrama,
+  xcidic,
+};
